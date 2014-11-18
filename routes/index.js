30121c4f4cfaace59e05c654a8ce22307b073ee2
Craig Horsborough <craighorsborough@gmail.com>
2014-11-18 16:15:18 +0000

    Playing with node

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res) {
+	res.render('helloworld')
+});
+
+router.get('/test', function(req, res) {
+	res.render('test')
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+module.exports = router;
